d654330c740c1a9ee37783ffb4ca31465c64286d
vimal Raveendran <vimalraveendra@gmail.com>
2020-02-07 11:52:51 +0100

    created Card component & Card styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import Card from "./components/card/Card";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Custom Hooks</h1>
+      <Card>
+        <h3>Custom Hooks</h3>
+      </Card>
     </div>
   );
 }
